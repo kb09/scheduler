7ae4fef1192b59b933896060c83ee7bf8c5747f3
kartik <kabakshi@hotmail.ca>
2021-12-08 18:16:14 -0500

    Add an event handler to the li

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,17 +3,23 @@ import classNames from "classnames";
 import "components/interviwerListItem.scss";
 
 export default function interviwerListItem(props){
-  // const interviewerClass = classNames('interviewer__item',{
-  //   'interviewer__item--selected': props.selected //The InterviewerListItem needs a prop to know if it is selected
-  // })
+  const interviewerClass = classNames('interviewer__item',{
+    'interviewer__item--selected': props.selected, //The InterviewerListItem needs a prop to know if it is selected
+    'interviewer__item--image': props.avatar,
+  });
+ 
+  //When clicked, the setInterviewer function should run, taking the interviewer id as a parameter
   return (
-  <li className="interviewers__item"> 
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.name)} key={props.id}> 
       <img
-        className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
-      />
-      Sylvia Palmer
+      className={'interviewer__item--image'}
+      src={props.avatar}
+      alt={props.name}
+      
+      
+      
+      /> 
+      {props.selected && props.name}
     </li>
   );
 } 
