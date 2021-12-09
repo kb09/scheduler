894bd919d58c05b3ae6acb1d2276346e99d2e19b
kartik <kabakshi@hotmail.ca>
2021-12-09 01:33:12 -0500

    add show component and confirm compnent

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,8 @@ import InterviewerList from 'components/InterviewerList';
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 
 
@@ -143,7 +145,10 @@ storiesOf("Button", module)
   <Appointment time="12pm" />))
   .add("Header", () => <Header time="12pm" />)
   .add('Empty', () => <Empty onAdd={action('onAdd')} />)
+  .add('Show', () => <Show student='Lydia Miller-Jones' interviewer={ interviewer } onEdit={action('onEdit')} onDelete={action('onDelete')}  />)
+  .add('Confirm', () => <Confirm onCancel={action('onCancel')} onConfirm={action('onConfirm')} />)
 
-
+  
+  
 
   
