69af5910a64d317272ec37fb155445abc3971fb2
kartik <kabakshi@hotmail.ca>
2021-12-11 15:54:38 -0500

    adding Transition with Replace

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,8 +5,27 @@ export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]); 
 
 
-  function transition() { /* ... */ }
-  function back() { /* ... */ }
+  
+  function transition(modeNew, replace = false) { 
+    if (!replace) {
+      setMode(modeNew);
+      history.push(modeNew);     
+    }
+    else {
+      setMode(modeNew);
+    }
+  }
+
+  const back = () => {
+    if (history.length > 1) {
+      const newHistory = [...history];
+      newHistory.pop();
+
+      setHistory(newHistory);
+
+      setMode(newHistory[newHistory.length - 1]);
+    }
+  };
 
   return { mode, transition, back };
 };
