da968bf3a311a6039a21ac7265954e38152664a0
kartik <kabakshi@hotmail.ca>
2021-12-11 15:31:41 -0500

    Add an empty back() function to the Hook that allows us to go back to the previous mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,5 +1,10 @@
+import { useState } from "react";
+
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
 
-  return { mode };
-}
+  function transition() { /* ... */ }
+  function back() { /* ... */ }
+
+  return { mode, transition, back };
+};
