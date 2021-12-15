9fe1e62bf9f1dca324042c501ee5b4ea03bc1303
kartik <kabakshi@hotmail.ca>
2021-12-15 15:09:03 -0500

    fixing bug on dayList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,21 +3,26 @@ import "components/DayListItem.js"
 import DayListItem from "components/DayListItem.js";
 
 
-// Our <DayList> component will take in three props.
+// // Our <DayList> component will take in three props.
 
-// days:Array an array of objects (each object represents a day and includes an id, name, and spots)
-// day:String the currently selected day
-// setDay:Function sets the currently selected day and accepts the name of the day eg. "Monday", "Tuesday"
+// // days:Array an array of objects (each object represents a day and includes an id, name, and spots)
+// // day:String the currently selected day
+// // setDay:Function sets the currently selected day and accepts the name of the day eg. "Monday", "Tuesday"
 
-export default function DayList({ days, day, setDay }) {
-  const parsedDayList = days.map(day => (
-    <DayListItem
-      key={day.id}
-      name={day.name}
-      spots={day.spots}
-      selected={day.name === day}
-      setDay={setDay}
-    />
-  ));
-  return <ul>{parsedDayList}</ul>;
+export default function DayList(props) {
+  const schedule = props.days.map(dayObj => {
+ 
+    return (
+      <DayListItem
+        key={dayObj.id}
+        name = {dayObj.name}
+        spots = {dayObj.spots}
+        selected={dayObj.name === props.value}
+        setDay={props.onChange}
+        {...dayObj}
+      />
+    )
+  })
+  return <ul>{schedule}</ul>
 }
+
