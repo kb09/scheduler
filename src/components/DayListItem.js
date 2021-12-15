a20e696932b330d709edcdad800c1a1af5921f35
kartik <kabakshi@hotmail.ca>
2021-12-15 17:56:17 -0500

    Navigating Days testing

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -11,7 +11,7 @@ export default function DayListItem(props) {
     'day-list__item--full' : props.spots === 0
   })
     return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}selected={props.selected}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}selected={props.selected} data-testid="day">
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light"> {formatSpots(props.spots)}</h3>
     </li>
